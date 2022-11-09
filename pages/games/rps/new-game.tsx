b47c9b905b73bc4698a68488b37d8c0fa7d59a70
sourabhxyz <swasti@sourabh.xyz>
2022-11-09 19:09:11 +0530

    improved styling

--- pages/games/rps/new-game.tsx
+++ pages/games/rps/new-game.tsx
@@ -117,11 +117,11 @@ const createGame = async (lucid: Lucid, router: NextRouter, playerBAddress: stri
       pathname: '/games/rps/game',
       query: {
         policyId,
-        player: 0,  // 0 for playerA and 1 for playerB
+        player: 'A',
       }
     })
   } catch (e) {
-    alert("Game could not be created: " + e) 
+    alert("Game could not be created: " + e)
   }
 }
 
@@ -154,7 +154,7 @@ const NewGame: NextPage = () => {
   const moves: Move[] = ["Rock", "Paper", "Scissors"]
   const createGameSchema = yup.object().shape({
     playerB: yup.string().required("Please enter other player's address"),
-    stake: yup.number().required("Please enter stake ada amount").integer().min(2, "Atleast 2 ada must be staked"),
+    stake: yup.number().required("Please enter stake Ada amount").integer().min(2, "Atleast 2 Ada must be staked"),
     move: yup.string().required("Please enter your move").oneOf(moves)
   })
   return (
@@ -172,28 +172,28 @@ const NewGame: NextPage = () => {
         >
           {(props) => (
             <Form>
-              <FormControl isInvalid={!!props.errors.playerB && props.touched.playerB} mt='7px' borderColor='black'>
-                <FormLabel>Enter the address of second player</FormLabel>
-                <Field as={Input} name='playerB' type='text' />
+              <FormControl isInvalid={!!props.errors.playerB && props.touched.playerB} borderColor='black'>
+                <FormLabel textAlign='center' fontWeight='bold'>Enter the address of second player</FormLabel>
+                <Field as={Input} name='playerB' type='text' placeholder='addr...'/>
                 <FormErrorMessage>{props.errors.playerB}</FormErrorMessage>
               </FormControl>
-              <FormControl isInvalid={!!props.errors.stake && props.touched.stake} mt='7px' borderColor='black'>
-                <FormLabel>Enter ada stake amount</FormLabel>
+              <FormControl isInvalid={!!props.errors.stake && props.touched.stake} mt='18px' borderColor='black'>
+                <FormLabel textAlign='center' fontWeight='bold'>Enter Ada stake amount</FormLabel>
                 <Field as={Input} name='stake' type='number' />
                 <FormErrorMessage>{props.errors.stake}</FormErrorMessage>
               </FormControl>
-              <FormControl isInvalid={!!props.errors.move && props.touched.move} mt='7px' borderColor='black'>
-                <FormLabel>Enter your move</FormLabel>
+              <FormControl isInvalid={!!props.errors.move && props.touched.move} mt='18px' borderColor='black'>
+                <FormLabel textAlign='center' fontWeight='bold'>Enter your move</FormLabel>
                 <Field as={RadioGroup} name='move'>
-                  <HStack>
-                    {moves.map((elem, ix) => (<Field as={Radio} key={ix} value={elem}>{elem}</Field>))}
-                  </HStack>
+                  <Flex justify='space-evenly' >
+                    {moves.map((elem, ix) => (<Field as={Radio} key={ix} value={elem} borderColor='black' _checked={{bg: 'black'}} >{elem}</Field>))}
+                  </Flex>
                 </Field>
                 <FormErrorMessage>{props.errors.move}</FormErrorMessage>
               </FormControl>
               <Flex justify='center'>
                 <Button
-                  mt={4}
+                  mt={'18px'}
                   {...brandButtonStyle}
                   isLoading={props.isSubmitting}
                   type='submit'
